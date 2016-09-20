9f08a8ba26f044d45a55ca41c21703778188d616
alain tiemblo <ninsuo@gmail.com>
2016-09-20 15:43:03 +0200

    removed useless things in the layout to lighten it

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Form\Extension\Core\Type;
 
 class DefaultController extends BaseController
 {
@@ -18,9 +18,28 @@ class DefaultController extends BaseController
      */
     public function indexAction(Request $request)
     {
-//        $list = $this->get('app.google')->listRooms();
-//        \Symfony\Component\VarDumper\VarDumper::dump($list);
-//        die();
+        $rooms    = $this->get('app.google')->listRooms();
+        $criteria = $request->get('criteria');
+
+        if (!is_null($criteria)) {
+            foreach ($rooms as $email => $name) {
+                if (false === strpos($name, $criteria)) {
+                    unset($rooms[$email]);
+                }
+            }
+        }
+
+        $filter = $this
+           ->createFormBuilder(['criteria' => $criteria])
+           ->setMethod('GET')
+           ->add('criteria', TextType::class, ['label' => 'Filter rooms'])
+           ->add('Go', 'submit')
+           ->getForm()
+           ->createView()
+        ;
+
+        return ['filter' => $filter, 'rooms' => $rooms];
+
 
 //        $list = $this->get('app.google')->listEvents('comuto.com_373335383233312d383638@resource.calendar.google.com');
 //        $free = $this->get('app.calendar')->getStatus($list);
@@ -44,7 +63,5 @@ class DefaultController extends BaseController
 //            $calendars[$entry->getId()] = $entry->getSummary();
 //        }
 
-
-        return array();
     }
 }
--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -7,21 +7,4 @@ use Knp\Menu\FactoryInterface;
 
 class Builder extends BaseMenu
 {
-   public function userLeftMenu(FactoryInterface $factory, array $options)
-    {
-        $menu = $this->createMenu($factory, parent::POSITION_LEFT);
-        $this->addRoute($menu, 'quickstart.menu.home', 'home');
-
-
-
-
-        /*
-          $this->addSubMenu($menu, 'test');
-          $this->addRoute($menu['test'], 'testA', 'testa');
-          $this->addRoute($menu['test'], 'testB', 'testb', array(), array(), true);
-          $this->addRoute($menu['test'], 'testC', 'testc');
-        */
-
-        return $menu;
-    }
 }
