6456fa32b0493fa564275325c1a999d7b83ed62c
Alain Tiemblo <alain.tiemblo@blablacar.com>
2016-09-29 13:35:38 +0200

    removed meeting details, added owner picture

---
+++ src/AppBundle/Twig/Extension/PeopleExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+class PeopleExtension extends \Twig_Extension
+{
+    protected $photoUrl;
+
+    public function __construct($photoUrl)
+    {
+        $this->photoUrl = $photoUrl;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('peoplePicture', [$this, 'peoplePicture']),
+            new \Twig_SimpleFilter('peopleName', [$this, 'peopleName']),
+        ];
+    }
+
+    public function peoplePicture($email)
+    {
+        return $this->photoUrl . md5(strtolower(trim($email)));
+    }
+
+    public function peopleName($email)
+    {
+        $tokens = explode('.', $email);
+
+        return ucfirst($tokens[0]);
+    }
+
+    public function getName()
+    {
+        return 'people';
+    }
+}
