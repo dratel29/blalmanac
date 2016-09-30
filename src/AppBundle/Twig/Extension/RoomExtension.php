5d3dfa829a5b99cacfcd9c5e6ac1a2b439b38016
Alain Tiemblo <alain.tiemblo@blablacar.com>
2016-09-30 14:30:23 +0200

    bigger fonts

--- src/AppBundle/Twig/Extension/RoomExtension.php
+++ src/AppBundle/Twig/Extension/RoomExtension.php
@@ -8,29 +8,19 @@ class RoomExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFilter('roomName', [$this, 'roomName']),
-            new \Twig_SimpleFilter('roomCountry', [$this, 'roomCountry']),
             new \Twig_SimpleFilter('roomSeats', [$this, 'roomSeats']),
         ];
     }
 
     public function roomName($roomName)
     {
-        if ($this->roomCountry($roomName)) {
+        if (preg_match('!^\[..\]!', $roomName)) {
             $roomName = trim(preg_replace('/[0-9]+/', '', substr($roomName, 4)), ' -');
         }
 
         $tokens = explode('/', $roomName);
 
         return reset($tokens);
-    }
-
-    public function roomCountry($roomName)
-    {
-        $country = substr($roomName, 1, 2);
-        $flag    = __DIR__.'/../../../Fuz/QuickStartBundle/Resources/public/img/countries/'.$country.'.png';
-        if (file_exists($flag)) {
-            return 'img/countries/'.$country.'.png';
-        }
     }
 
     public function roomSeats($roomName)
